d857e32eb4d1860e2cfdc8b6b16bf08ce4e610b1
Pranay <pranaysarkar22gmail.com>
2021-07-20 01:09:16 +0200

    na: Coffeeshop identifier added for authentication with auth0

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,10 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    url: 'dev-3ncdr6eo.eu', // the auth0 domain prefix
+    audience: 'coffeeShop', // the audience set for the auth0 app
+    clientId: 'IcZWvybw42PUruxtP6zTtvDY2OD9eNzz', // the client id generated for the auth0 app
+    //callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    callbackURL: 'https://127.0.0.1:3000/tabs/user-page'
   }
 };
