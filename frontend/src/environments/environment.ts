08f735c182b1917cc02dad049a8614b3bc8b94eb
Pranay Sarkar <pranaysarkar22@gmail.com>
2021-10-19 18:46:44 +0200

    environment .ty updated with callback url

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -9,8 +9,8 @@ export const environment = {
     url: 'dev-3ncdr6eo.eu', // the auth0 domain prefix
     audience: 'coffeeShop', // the audience set for the auth0 app
     clientId: 'IcZWvybw42PUruxtP6zTtvDY2OD9eNzz', // the client id generated for the auth0 app
-    //callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
-    callbackURL: 'https://127.0.0.1:3000/tabs/user-page'
+    callbackURL: 'http://localhost:8100' // the base url of the running ionic application. 
+    //callbackURL: 'https://127.0.0.1:3000/tabs/user-page'
   }
 };
 
